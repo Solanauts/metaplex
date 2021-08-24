954c55196f5f0b8712b2234cfb49bbfd17772030
Brian Lewis <eoscrypto87@protonmail.com>
2021-08-24 17:10:14 -0400

    Added Payment functionality

--- js/packages/web/src/components/SolanautLaunchCard/index.tsx
+++ js/packages/web/src/components/SolanautLaunchCard/index.tsx
@@ -2,6 +2,7 @@ import React, {useMemo, useState} from "react"
 import {Connection, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction} from "@solana/web3.js";
 import {Alert, Button, Form, Input, Space, Typography} from 'antd';
 import {LoadingOutlined, RedoOutlined} from '@ant-design/icons';
+import {useWallet} from "@oyster/common/dist/lib/contexts/wallet";
 //import { getPhantomWallet } from '@solana/wallet-adapter-wallet'
 //import {DEFAULT} from '../../../../common/src/contexts/connection'
 
@@ -25,8 +26,8 @@ const Transfer = ({ keypair }) => {
 
   const generate = () => {
     const address = '8VG1sDEF9UMpTrQseCc1R3ZWHnQDB73jS83NBzVujELk';
-    const owner = address.toString();
-    setToAddress(owner);
+    //const owner = address.toString();
+    setToAddress(address);
   }
 
   // @ts-ignore
@@ -43,7 +44,7 @@ const Transfer = ({ keypair }) => {
 
     // @ts-ignore
     const toPubKey = new PublicKey(toAddress);
-    const fromPubKey = new PublicKey(values.from);
+    const fromPubKey = new PublicKey(useWallet);
 
     const instructions = SystemProgram.transfer({
       fromPubkey: fromPubKey,
@@ -87,13 +88,7 @@ const Transfer = ({ keypair }) => {
       name="transfer"
       layout="horizontal"
       onFinish={transfer}
-      initialValues={{
-        from: keypair.publicKey.toString()
-      }}
     >
-      <Form.Item label="Sender" name="from" required>
-        <Text code>{keypair.publicKey.toString()}</Text>
-      </Form.Item>
 
       <Form.Item label="Amount" name="amount" required tooltip="1 lamport = 0.000000001 SOL">
         <Space direction="vertical">
