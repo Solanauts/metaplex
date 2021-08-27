b389b83725be13ab42c1d255ce07265e59a09bd1
Brian Lewis <eoscrypto87@protonmail.com>
2021-08-27 15:58:23 -0400

    Added Beginning Context API and Chain of Responsibility

--- js/packages/web/src/components/SolanaLaunchContainer/SolanautsLaunchContainer.tsx
+++ js/packages/web/src/components/SolanaLaunchContainer/SolanautsLaunchContainer.tsx
@@ -1,9 +1,42 @@
 import SolanautLaunchCard  from "../SolanautLaunchCard";
+import SolanautContext from "../../contexts/meta/solanautContext";
 
 function SolanautsLaunchContainer ()
 {
+  /*const Test = () => (
+    <>
+      <div>
+        <p>Test 1</p>
+      </div>
+    </>
+  );*/
+  const transferNFT = {
+    receive(data: string): string{
+      console.log('The NFT has been transferred to the collector: ', data)
+      return data;
+    }
+  }
+  const removeRights = {
+    // Remove rights will have to return both PublicKey and NFT to TransferNFT
+    notify(data: string): string {
+      console.log('Full rights have been removed: ', data)
+      return transferNFT.receive(data);
+    }
+  }
+  const findNFT = {
+    notify(data: string): string{
+      console.log('The NFT has been found: ', data)
+      return removeRights.notify(data);
+    }
+  }
+
   return (
-    <SolanautLaunchCard />
+    <SolanautContext.Provider value={{isPaid: false}}>
+        <SolanautLaunchCard />
+        <div>
+          { findNFT.notify('Solanaut Launchpad') }
+        </div>
+    </SolanautContext.Provider>
   )
 }
 export default SolanautsLaunchContainer;
---
+++ js/packages/web/src/contexts/meta/solanautContext.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SolanautContext = React.createContext({
+  isPaid: false,
+});
+
+export default SolanautContext;
